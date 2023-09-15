3087e7148cb745c48057c63c1edd770d5ff88262
WoojinKim1225 <96871618+WoojinKim1225@users.noreply.github.com>
2023-09-15 16:38:56 +0900

    Feat: Making Mirror Reflection

---
+++ Assets/Scripts/Woojin/Mirror/DetectHitPosition.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectHitPosition : MonoBehaviour
+{
+    [SerializeField] private Transform _bulletStartWS;
+    [SerializeField] private Transform _bulletEndWS;
+}
