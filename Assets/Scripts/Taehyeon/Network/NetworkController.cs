ea19a839dc69f0e785c84705bebc9d20b0992aed
taehyeoon <tedxpem@naver.com>
2023-09-24 10:58:29 +0900

    Feat: synchronize current connected player number
    연결이 끊겼을 때도 동기화 완료
    캐릭터 프리팹으로 관리

--- Assets/Scripts/Taehyeon/Network/NetworkController.cs
+++ Assets/Scripts/Taehyeon/Network/NetworkController.cs
@@ -4,18 +4,22 @@ using Logger = Utils.Logger;
 
 public class NetworkController : SingletonNetworkPersistent<NetworkController>
 {
-    private NetworkVariable<int> _curPlayerNum = new(0);
     public string joinCode;
 
     public override void OnNetworkSpawn()
     {
         NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisConnected;
     }
 
     public override void OnNetworkDespawn()
     {
         NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisConnected;
+
         if (IsServer)
         {
             Logger.Log("server disconnected");
@@ -26,18 +30,31 @@ public class NetworkController : SingletonNetworkPersistent<NetworkController>
     {
         if (IsServer)
         {
-            _curPlayerNum.Value += 1;
-            SetPlayerNumClientRpc(NetworkManager.Singleton.ConnectedClients.Count);
-            Logger.Log($"player {_curPlayerNum.Value} connected");
-            if (_curPlayerNum.Value == GameData.playerNumPerTeam * 2)
+            GameData.currentConnectedPlayerNum += 1;
+            SetPlayerNumClientRpc(GameData.currentConnectedPlayerNum);
+            Logger.Log($"player {clientId} connected");
+            
+            // Move to next scene when all players are connected
+            if (GameData.currentConnectedPlayerNum == GameData.playerNumPerTeam * 2)
             {
                 NetworkManager.Singleton.SceneManager.LoadScene("BattleScene", LoadSceneMode.Single);
             }
         }
     }
     
+    private void OnClientDisConnected(ulong clientId)
+    {
+        if (IsServer)
+        {
+            GameData.currentConnectedPlayerNum -= 1;
+            SetPlayerNumClientRpc(GameData.currentConnectedPlayerNum);
+            Logger.Log($"player {clientId} disConnected");
+        }
+    }
+    
+    
     [ClientRpc]
-    public void SetPlayerNumClientRpc(int playerNum)
+    private void SetPlayerNumClientRpc(int playerNum)
     {
         GameData.currentConnectedPlayerNum = playerNum;
     }
