c45ca6179c952773e27e0bff20879bf34bd4a0c1
jaewonjung6446 <jaewon6446@naver.com>
2023-10-03 14:09:52 +0900

    TestScene

---
+++ Assets/Scripts/Jaewon/PlayerManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using Unity.Mathematics;
+
+public class PlayerManager : MonoBehaviour
+{
+    public MovSystem movSystem;
+}
