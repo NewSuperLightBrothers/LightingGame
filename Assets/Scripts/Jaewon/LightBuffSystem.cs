7ec8ad37da1e17af4794eec37584d65aa0b8de46
jaewonjung6446 <jaewon6446@naver.com>
2023-10-08 16:57:32 +0900

    빛 잔상 버프 효과 구현

--- Assets/Scripts/Jaewon/LightBuffSystem.cs
+++ Assets/Scripts/Jaewon/LightBuffSystem.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using KinematicCharacterController;
+using KinematicCharacterController.Examples;
+
 
 public class LightBuffSystem : MonoBehaviour
 {
@@ -9,10 +13,21 @@ public class LightBuffSystem : MonoBehaviour
     private List<GameObject> _lightList = new List<GameObject>();
     public bool isEnemy;
     public bool isAlly;
+    public Image stateCheck;
+    [Header("Buff Velocity")]
+    private float _defaultVelocity = 6;
+    private float _buffVelocity = 10;
+    private float _debuffVelocity = 3;
+    Color redColor = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+    Color blueColor = new Color(0.0f, 0.0f, 1.0f, 1.0f);
+    Color pupleColor = new Color(1.0f, 0.0f, 1.0f, 1.0f);
+    Color defaultColor = new Color(0, 0, 0, 1.0f);
+    ExampleCharacterController exampleCharacterController;
     private void Start()
     {
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
+        exampleCharacterController = GetComponent<ExampleCharacterController>();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -52,10 +67,8 @@ public class LightBuffSystem : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (isAlly || isEnemy)
-        {
-            StartCoroutine(Buff());
-        }
+        StartCoroutine(Buff());
+        Debug.Log("현재 최대 속도 = " + exampleCharacterController.MaxStableMoveSpeed);
     }
     //불러온 에셋에서 velocity를 변환하는 메소드를 찾아야함. 찾으면 바로 적용가능
     private IEnumerator Buff()
@@ -66,13 +79,13 @@ public class LightBuffSystem : MonoBehaviour
             while (true)
             {
                 time += Time.deltaTime;
-                this.rb.velocity *= 0.5f;
+                exampleCharacterController.MaxStableMoveSpeed = _debuffVelocity;
                 if (time >= 1.0f)
                 {
-                    this.rb.velocity /= 0.5f;
+                    time = 0;
+                    exampleCharacterController.MaxStableMoveSpeed = _defaultVelocity;
                     break;
                 }
-                Debug.Log("속도 감소");
                 yield return null;
             }
         }
@@ -81,20 +94,19 @@ public class LightBuffSystem : MonoBehaviour
             while (true)
             {
                 time += Time.deltaTime;
-                this.rb.velocity *= 1.5f;
+                exampleCharacterController.MaxStableMoveSpeed = _buffVelocity;
                 if (time >= 1.0f)
                 {
-                    this.rb.velocity /= 1.5f;
+                    time = 0;
+                    exampleCharacterController.MaxStableMoveSpeed = _defaultVelocity;
                     break;
                 }
-                Debug.Log("속도 증가");
                 yield return null;
             }
         }
         if(isAlly && isEnemy)
         {
             yield return null;
-            Debug.Log("중첩 상태");
         }
     }
 }
