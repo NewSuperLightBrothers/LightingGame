b614a7640a9d3d32b3e00bc5eacf82f08d95582c
jaewonjung6446 <jaewon6446@naver.com>
2023-10-30 19:15:16 +0900

    애니메이션 추가
    Ybot기반 애니메이션 추가(걷기, 달리기, 이동 중 공격, 서서 공격)

---
+++ Assets/Scripts/Jaewon/AnimateController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KinematicCharacterController;
+public class AnimateController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] KinematicCharacterMotor kinematicCharacterMotor;
+    static float velocity;
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        Run();
+    }
+    private void Run()
+    {
+        velocity = kinematicCharacterMotor.Velocity.magnitude;
+        Debug.Log("¼Óµµ = " + velocity);
+        animator.SetFloat("Velocity", velocity);
+    }
+    public void Atk()
+    {
+        animator.SetTrigger("Attack");
+    }
+}
