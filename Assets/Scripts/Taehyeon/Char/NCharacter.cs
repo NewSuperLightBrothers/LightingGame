38b82d444a7eed04fcbdc26deb54c04f795e634b
taehyeoon <tedxpem@naver.com>
2023-10-31 18:25:04 +0900

    Feat: 타겟 생성 네트워크 연동 확인

--- Assets/Scripts/Taehyeon/Char/NCharacter.cs
+++ Assets/Scripts/Taehyeon/Char/NCharacter.cs
@@ -4,10 +4,36 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using Logger = Utils.Logger;
+using Random = UnityEngine.Random;
 
 public class NCharacter : NetworkBehaviour
 {
     public Joystick joystick;
+
+    public NetworkVariable<myStruct> nint = new NetworkVariable<myStruct>(new myStruct(), NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner);
+
+    public struct myStruct : INetworkSerializable
+    {
+        public int aint;
+        public bool bbool;
+        public NetworkString sss;
+        
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref aint);
+            serializer.SerializeValue(ref bbool);
+            serializer.SerializeValue(ref sss);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        nint.OnValueChanged += ((value, newValue) =>
+        {
+            Logger.Log(OwnerClientId + " / aint : " +  nint.Value.aint + " / bbool : " + nint.Value.bbool + " / sss : " + nint.Value.sss);
+        });
+    }
 
     private void Update()
     {
@@ -15,6 +41,19 @@ public class NCharacter : NetworkBehaviour
         {
             if (IsOwner)
             {
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    // nint.Value = new myStruct
+                    // {
+                    //     aint = Random.Range(0, 100),
+                    //     bbool = !nint.Value.bbool,
+                    //     sss = nint.Value.sss + "1"
+                    // };
+                    
+                    TestServerRPC(new ServerRpcParams());
+                }
+
+                // Logger.Log("player id : " + OwnerClientId + " random val : " + nint.Value);
                 if (joystick.Horizontal > 0.5f)
                 {
                     transform.Translate(Vector3.right * Time.deltaTime * 5f);
@@ -34,6 +73,11 @@ public class NCharacter : NetworkBehaviour
                 }
             }
         }
-            
+    }
+
+    [ServerRpc]
+    public void TestServerRPC(ServerRpcParams rpcParams)
+    {
+        Logger.Log(OwnerClientId + " / " + rpcParams.Receive.SenderClientId);
     }
 }
